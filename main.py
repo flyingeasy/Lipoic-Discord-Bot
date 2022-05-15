8a1f0b1ce0df879a7d0dafa126cd43be2d09176a
flyingeasy <93474905+flyingeasy@users.noreply.github.com>
2022-05-15 18:43:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import discord , tokens
 # prefix: str = "!"
 # intents = discord.Intents.default()
 
+cogs = [
+  "hello"
+  ]
+
 bot: discord.Bot = discord.Bot()
 
 @bot.event
 async def on_ready():
   print("Log in as " + str(bot.user))
 
-for cog in [
-  "hello"
-  ]:
+for cog in cogs:
   bot.load_extension(cog)
 
 bot.run(tokens.bot)
